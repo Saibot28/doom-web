a4d2a8902e75be63177ca5ede90aeb7167755a1f
Saibot <tob.princen@gmail.com>
2025-11-01 17:07:39 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,1 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import mimetypes
 
-# Make sure .zip files have the correct MIME type
-mimetypes.add_type('application/zip', '.zip')
-
-# Set the host and port
-host = 'localhost'
-port = 8080
-
-# Create the HTTP server
-server = HTTPServer((host, port), SimpleHTTPRequestHandler)
-print(f"Serving HTTP on {host} port {port} (http://{host}:{port}/) ...")
-
-# Start serving
-server.serve_forever()
